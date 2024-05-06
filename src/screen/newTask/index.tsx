874fca8b3868105e56cb95fdb35c8ee3c5cfaa72
kjnhk1001 <kjn.hk10001@gmail.com>
2024-05-06 09:54:09 +0900

    feat：タスク更新API

--- src/screen/newTask/index.tsx
+++ src/screen/newTask/index.tsx
@@ -4,7 +4,7 @@ import { useRouter } from "next/navigation";
 import React from "react";
 import { useForm } from "react-hook-form";
 
-type FormValue = {
+export type FormValue = {
   title: string;
   description: string;
   dueDate: Date;
