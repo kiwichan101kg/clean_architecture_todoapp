a34c8bb4d9adda668bf763e8cd1b9db7029f6db0
kjnhk1001 <kjn.hk10001@gmail.com>
2024-06-03 22:53:35 +0900

    feat：Basic認証

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    BASIC_AUTH_USERNAME: process.env.BASIC_AUTH_USERNAME,
+  },
+};
 
 export default nextConfig;
